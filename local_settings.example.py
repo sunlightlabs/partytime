cc3f8e4b88169c8df5a4e6ef37640b745047139e
jcarbaugh <jcarbaugh@4cd83f71-0d2b-45f9-9593-fb53dc6ed8f1>
2008-10-22 15:41:35 +0000

    Moving files to frontend repo
    git-svn-id: https://source.sunlightlabs.com/partytime/frontend@191 4cd83f71-0d2b-45f9-9593-fb53dc6ed8f1

---
+++ local_settings.example.py
@@ -0,0 +1,19 @@
+DATABASE_ENGINE = ''
+DATABASE_NAME = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+TEMPLATE_DIRS = (
+    # full path to template dirs with trailing slashes
+)
+
+FILE_UPLOAD_PATH = "/dev/null" # no trailing slash
+
+COUCHDB_HOST = ''
+COUCHDB_PORT = 0
+
+PARTYTIME_DB = 'party_time_db'
+
+CACHE_BACKEND = 'locmem:///'
