c2544a36bf9279409543049c2499a499d551726c
lrosiak <lrosiak@4cd83f71-0d2b-45f9-9593-fb53dc6ed8f1>
2009-09-26 00:16:54 +0000

    added autocomplete
    git-svn-id: https://source.sunlightlabs.com/partytime/site@337 4cd83f71-0d2b-45f9-9593-fb53dc6ed8f1

--- publicsite/widgets.py
+++ publicsite/widgets.py
@@ -30,9 +30,9 @@ class ForeignKeySearchInput(forms.HiddenInput):
 			'all': ('%s/jquery.autocomplete.css' % settings.MEDIA_URL,)
 		}
 		js = (
-			'%s/js/jquery.js' % settings.MEDIA_URL,
-			'%s/js/jquery.autocomplete.js' % settings.MEDIA_URL,
-			'%s/autocomplete/AutocompleteObjectLookups.js' % settings.MEDIA_URL
+			'%s/jquery.js' % settings.MEDIA_URL,
+			'%s/jquery.autocomplete.js' % settings.MEDIA_URL,
+			'%s/AutocompleteObjectLookups.js' % settings.MEDIA_URL
 		)
 
 	def label_for_value(self, value):
@@ -119,12 +119,12 @@ class ManyToManySearchInput(forms.MultipleHiddenInput):
 	"""
 	class Media:
 		css = {
-			'all': ('%s/jquery-autocomplete/jquery.autocomplete.css' % settings.MEDIA_URL,)
+			'all': ('%s/jquery.autocomplete.css' % settings.MEDIA_URL,)
 		}
 		js = (
 			'%s/jquery.js' % settings.MEDIA_URL,
-			'%s/jquery-autocomplete/jquery.autocomplete.js' % settings.MEDIA_URL,
-			'%s/autocomplete/AutocompleteObjectLookups.js' % settings.MEDIA_URL
+			'%s/jquery.autocomplete.js' % settings.MEDIA_URL,
+			'%s/AutocompleteObjectLookups.js' % settings.MEDIA_URL
 		)
 
 
