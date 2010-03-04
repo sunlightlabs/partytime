1d35e5f9114137044f1cb9aa8de637bd3431a668
James Turk <james.p.turk@gmail.com>
2010-03-04 18:14:47 -0500

    latest event in action

--- publicsite/views.py
+++ publicsite/views.py
@@ -460,7 +460,11 @@ class PartyTimeLayar(LayarView):
     def poi_from_partytime_item(self, item):
         venue_url = 'http://politicalpartytime.org/search/Venue_Name/%s/' % item.venue_name
         actions = [{'label': item.venue_name, 'uri':venue_url}]
+        latest_event = Event.objects.filter(venue=item.id).select_related().order_by('-start_date')[0]
         return POI(id=item.id, lat=item.latitude, lon=item.longitude,
-                   title=item.venue_name, actions=actions)
+                   title=item.venue_name, line2=item.venue_address(),
+                   line2=' '.join(latest_event.beneficiaries.all()),
+                   attribution='http://PoliticalPartyTime.org',
+                   actions=actions)
 
 partytime_layar = PartyTimeLayar()
