d39010f386d4512ee8e319f05e762d223d24d6e5
Aaron Bycoffe <abycoffe@sunlightfoundation.com>
2010-07-06 13:52:41 -0400

    Fix broken legislator images.

--- publicsite/views.py
+++ publicsite/views.py
@@ -194,7 +194,7 @@ def polwithpac(request, cid):
             lm = ll
       
     eventlist = Event.objects.filter(status='', beneficiaries__crp_id=cid).order_by('-start_date','-start_time')
-    return render_to_response('publicsite/polwithpac.html', {"eventlist":eventlist, "lm": lm, "pacname": pacname })
+    return render_to_response('publicsite/polwithpac.html', {"eventlist":eventlist, "lm": lm, "pacname": pacname, "snapshot_image_name": '' })
     
 #
 # file uploading
