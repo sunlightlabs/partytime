b51ed0b85272ad421a11d5019f56956bf7522011
Jacob Fenton <jfenton@sunlightfoundation.com>
2012-08-27 17:17:32 -0400

    add script to fix rnc convention names

---
+++ publicsite/scripts/rename_rnc_events.py
@@ -0,0 +1,32 @@
+""" Hack to count appearance of lawmaker ids so we can kill unused dupes. """
+
+import sys, re
+
+from django.core.management import setup_environ
+sys.path.append('/projects/partytime/src/partytime/')
+#sys.path.append('/Users/jfenton/partytime/partytime')
+
+import settings
+setup_environ(settings)
+
+from publicsite.models import *
+
+from dateutil.parser import parse as dateparse
+
+
+#events = Event.objects.daterange('20120825', '20120902')
+
+events = Event.objects.daterange('20120625', '20120902')
+
+convention_event = re.compile("\s*Republican Convention --\s*", re.I)
+
+for event in events:
+    match = re.match(convention_event, event.entertainment)
+    if match:
+        
+        print 'Found convention event: %s - %s - %s ' % (event.entertainment, event.start_date, event.venue.venue_name)
+        
+        fixed_name = re.sub(convention_event, "", event.entertainment)
+        fixed_name = fixed_name + " -- Republican Convention"
+        
+        print "Proposed replacement name: %s" % (fixed_name)
